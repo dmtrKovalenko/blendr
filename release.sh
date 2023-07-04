cc54f0b1cf75ea325211ff081758aa1994e41a12
Dmitriy <dmtr.kovalenko@outlook.com>
2023-07-04 21:28:31 +0200

    chore(utils): Add release script

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION="$1"
+
+cargo set-version "$VERSION"
+
+git add Cargo.toml Cargo.lock
+
+git commit -m "chore(release): v$VERSION"
+
+git tag "v$VERSION"
+
+git push origin "v$VERSION"
+
+git push
